4881f9888a7891665d479fc5a39250c7e0fdd7fc
Yeeun Kim <maemae2@naver.com>
2022-10-25 19:18:06 +0900

    221025 HashMapExample2

---
+++ src/ch13/Student.java
@@ -0,0 +1,27 @@
+package ch13;
+
+import java.util.Objects;
+
+// 키로 사용할 객체 - hashCode()와 equals() 재정의
+public class Student {
+    public int sno;
+    public String name;
+
+    public Student(int sno, String name) {
+        this.sno = sno;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return sno == student.sno && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sno, name);
+    }
+}
