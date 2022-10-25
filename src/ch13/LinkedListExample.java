cb99f48cb424aee2df74016ca04186bf2a09d9bd
Yeeun Kim <maemae2@naver.com>
2022-10-25 14:28:24 +0900

    221025 LinkedListExample

---
+++ src/ch13/LinkedListExample.java
@@ -0,0 +1,38 @@
+package ch13;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        List<String> linkedList = new LinkedList<>();
+
+        long startTime;
+        long endTime;
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            arrayList.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.println("ArrayList 걸린 시간 : ");
+        System.out.println( (endTime - startTime) + "ns");
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            linkedList.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList 걸린 시간 : ");
+        System.out.println( (endTime - startTime) + "ns");
+
+//        ArrayList 걸린 시간 :
+//        5640000ns
+//        LinkedList 걸린 시간 :
+//        1089900ns
+//        => 거의 5배 차이가 남
+
+    }
+}
